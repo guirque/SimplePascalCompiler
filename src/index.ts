55e143cdba3dec4035ebedf53de8ea0bef36036f
Guilherme <guirque@gmail.com>
2024-10-31 15:26:35 -0300

    build: adicionando porta default e porta por env em docker-compose

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ app.use(Express.json());
 app.use(Express.urlencoded({extended: true}));
 app.use(router);
 
-app.listen(process.env.PORT, ()=> {
-    console.log("<!> Server running on port " + process.env.PORT);
+app.listen(process.env.PORT ?? 3000, ()=> {
+    console.log("<!> Server running on port " + (process.env.PORT ?? 3000));
 })
